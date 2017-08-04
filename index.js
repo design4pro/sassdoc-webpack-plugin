e54791c4d235d003bd204494f7796b6b5ac55861
Rafał Wolak <rafal@tomandco.co.uk>
2017-08-04 15:21:43 +0200

    fix(package): change to emit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function SassDocPlugin(options) {
 SassDocPlugin.prototype.apply = function (compiler) {
   var self = this;
 
-  compiler.plugin('after-compile', function () {
+  compiler.plugin('emit', function () {
     sassdoc(self.options.source, self.options.config)
       .then(function () {
         console.log('Your documentation has been generated!');
